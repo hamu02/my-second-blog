f9812e548027f1c69f1e8db6ca6a38477c69f6d8
hamu02 <mi.itsuki.simone.s@icloud.com>
2024-08-27 17:38:14 +0900

    ミスを修正

--- blog/views.py
+++ blog/views.py
@@ -19,7 +19,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.publish_date = timezone.now()
+            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -29,11 +29,13 @@ def post_new(request):
 def post_edit(request,pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
-        form =PostForm(request.POST, intance=post)
+        form =PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
+            post.published_date = timezone.now()
             post.save()
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
